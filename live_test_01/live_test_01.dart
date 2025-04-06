7f6f2aae3940386b9c539a5adc73af762ca6cdc1
Fahim <fahimsafaat67@gmail.com>
2025-04-06 23:27:18 +0600

    live test 01

---
+++ live_test_01/live_test_01.dart
@@ -0,0 +1,33 @@
+void main() {
+  List<Map<String, dynamic>> students = [
+    {"name": "Alice", "scores": [85, 90, 78]},
+    {"name": "Bob", "scores": [88, 76, 95]},
+    {"name": "Charlie", "scores": [90, 92, 85]},
+  ];
+
+  Map<String, double> averages = {};
+
+  for (int i = 0; i < students.length; i++) {
+    String name = students[i]["name"];
+    List<int> scores = students[i]["scores"];
+
+    int total = 0;
+    for (int j = 0; j < scores.length; j++) {
+      total = total + scores[j];
+    }
+
+    double avg = total / scores.length;
+    // (rounded to 2 decimal places).
+    avg = double.parse(avg.toStringAsFixed(2));
+
+    averages[name] = avg;
+  }
+
+  List<MapEntry<String, double>> sortedList = averages.entries.toList();
+  sortedList.sort((a, b) => b.value.compareTo(a.value)); // High to low
+
+
+  for (int i = 0; i < sortedList.length; i++) {
+    print("${sortedList[i].key}: ${sortedList[i].value}");
+  }
+}
